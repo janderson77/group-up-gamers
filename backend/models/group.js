e327324fd82c57e52f2c2dd52b3d7bec2c24b486
James <jamesseventyseven@gmail.com>
2021-01-11 02:32:50 +0000

    added groups routes

--- backend/models/group.js
+++ backend/models/group.js
@@ -123,4 +123,4 @@ class Group {
     
 };
 
-module.exports = Company;
+module.exports = Group;
--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -3,17 +3,84 @@ const router = new express.Router();
 
 const {adminRequired, authRequired} = require("../middleware/auth");
 
+const Group = require('../models/group');
 const {validate} = require("jsonschema");
 
 const {groupNew, groupUpdate} = require('../schemas/index')
 
-router.get("/", authRequired, async function (req, res, next) {
+router.get("/", async function (req, res, next) {
     try {
-      const companies = await Company.findAll(req.query);
-      return res.json({companies});
+      const group = await Group.findAll(req.query);
+      return res.json({group});
+    }
+  
+    catch (e) {
+      return next(e);
+    }
+  });
+
+router.get("/:id", async function (req, res, next) {
+    try {
+      const group = await Group.findOneById(req.params.id);
+      return res.json({group});
+    }
+  
+    catch (e) {
+      return next(e);
+    }
+});
+
+router.post("/", async function (req, res, next) {
+    try {
+      const validation = validate(req.body, groupNew);
+  
+      if (!validation.valid) {
+        return next({
+          status: 400,
+          message: validation.eors.map(e => e.stack)
+        });
+      };
+  
+      const group = await Group.create(req.body);
+      return res.status(201).json({group});   // 201 CREATED
+    }catch (e) {
+      return next(e);
+    };
+});
+
+router.patch("/:id", async function (req, res, next) {
+    try {
+      if ("id" in req.body) {
+        return next({status: 400, message: "Not allowed"});
+      }
+  
+      const validation = validate(req.body, groupUpdate);
+      if (!validation.valid) {
+        return next({
+          status: 400,
+          message: validation.errors.map(e => e.stack)
+        });
+      }
+  
+      const group = await Group.update(req.params.handle, req.body);
+      return res.json({group});
     }
   
     catch (err) {
       return next(err);
     }
-  });
+});
+
+router.delete("/:id", async function (req, res, next) {
+    try {
+      await Group.remove(req.params.id);
+      return res.json({message: "Group deleted"});
+    }
+  
+    catch (err) {
+      return next(err);
+    }
+});
+
+
+module.exports = router;
